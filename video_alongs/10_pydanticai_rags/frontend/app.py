d459257db7b32eab263f076040ee2c3ae0a87941
Fredrik Kron <fkron95@gmail.com>
2025-12-01 16:43:22 +0100

    PydanticAI RAG

---
+++ video_alongs/10_pydanticai_rags/frontend/app.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import requests
+from pathlib import Path
+
+ASSETS_PATH = Path(__file__).absolute().parents[1] / "assets"
+
+def layout():
+
+    st.markdown("# RAGbit")
+    st.markdown("Ask a question about different dwarf rabbits")
+    text_input = st.text_input(label="Ask a question")
+
+    if st.button("Send") and text_input != "":
+        response = requests.post(
+            "http://127.0.0.1:8000/rag/query", json={"prompt": text_input}
+        )
+
+        data = response.json()
+
+        st.markdown("## Question:")
+        st.markdown(text_input)
+
+        st.markdown("## Answer:")
+        st.markdown(data["answer"])
+
+        st.markdown("## Source:")
+ 
+        st.image(ASSETS_PATH / f"{data['filename']}.png")
+
+        st.write("DEBUG data returned from API:", data)
+
+
+if __name__ == "__main__":
+    layout()
